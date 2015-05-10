77c6ca0d897e9f4518bd74a8d8303edfb569295a
Raul Roa <raul.roa@gmail.com>
2015-05-10 03:55:38 -0500

    Added a comment describing all the characters domain model properties.

--- src/Marvel.Api/Model/DomainObjects/Character.cs
+++ src/Marvel.Api/Model/DomainObjects/Character.cs
@@ -5,16 +5,59 @@ namespace Marvel.Api.Model.DomainObjects
 {
     public class Character
     {
+        /// <summary>
+        /// The unique ID of the character resource.
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// The name of the character.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// A short bio or description of the character.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// The date the resource was most recently modified.
+        /// </summary>
         public string Modified { get; set; }
+
+        /// <summary>
+        /// The canonical URL identifier for this resource.
+        /// </summary>
         public string ResourceURI { get; set; }
+
+        /// <summary>
+        /// A set of public web site URLs for the resource.
+        /// </summary>
         public List<Url> Urls { get; set; }
+
+        /// <summary>
+        /// The representative image for this character.
+        /// </summary>
         public Thumbnail Thumbnail { get; set; }
+
+        /// <summary>
+        /// A resource list containing comics which feature this character.
+        /// </summary>
         public ComicList Comics { get; set; }
+
+        /// <summary>
+        /// A resource list of stories in which this character appears.
+        /// </summary>
         public StoryList Stories { get; set; }
+
+        /// <summary>
+        /// A resource list of events in which this character appears.
+        /// </summary>
         public EventList Events { get; set; }
+
+        /// <summary>
+        /// A resource list of series in which this character appears.
+        /// </summary>
         public SeriesList Series { get; set; }
     }  
 }
